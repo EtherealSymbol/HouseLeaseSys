4d94d9e311ac99ec8325d0ca9b9ed1459ecb41ff
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:54:39 +0800

    Update PaidMapper.java

--- src/main/java/dao/PaidMapper.java
+++ src/main/java/dao/PaidMapper.java
@@ -6,8 +6,13 @@ import pojo.Paid;
 import pojo.QueryVo;
 
 public interface PaidMapper {
+	
 	public List<Paid> selectall(QueryVo vo);
+	
 	public Double selectsum(QueryVo vo);
+	
 	public void deletepaid(Integer id);
+	
 	public void insertpaid(Paid paid);
+	
 }
