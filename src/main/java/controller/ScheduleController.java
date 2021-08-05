1753d839609945a44ca49bc60de3c830c9bc7ec2
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:53:37 +0800

    Update ScheduleController.java

--- src/main/java/controller/ScheduleController.java
+++ src/main/java/controller/ScheduleController.java
@@ -17,41 +17,51 @@ import service.ScheduleService;
 @Controller
 @RequestMapping("/schedule")
 public class ScheduleController {
+	
 	@Autowired
 	private ScheduleService scheduleService;
 	
 	@RequestMapping("/selectAll")
-	public String selectAll(Model model ,@RequestParam(required=false,defaultValue="1") Integer page,
+	public String selectAll(Model model ,
+			@RequestParam(required=false,defaultValue="1") Integer page,
             @RequestParam(required=false,defaultValue="2") Integer pageSize){
-		 PageHelper.startPage(page, pageSize);
+		
+		PageHelper.startPage(page, pageSize);
 		List<Schedule> schedule=scheduleService.selectAll();
 		PageInfo<Schedule> p=new PageInfo<Schedule>(schedule);
 		model.addAttribute("schedule", schedule);
 		model.addAttribute("p", p);
 		model.addAttribute("mainPage", "schedule.jsp");
+		
 		return "admin/main1";
 	}
+	
 	@RequestMapping("/deleteschedule")
 	public String deleteschedule(Integer id){
 		scheduleService.deleteschedule(id);
+		
 		return "redirect:selectAll.action";
 	}
+	
 	@RequestMapping("/insertschedule")
-	public String insertschedule(Schedule schedule,Model model){
+	public String insertschedule(Schedule schedule, Model model){
 		scheduleService.insertschedule(schedule);
 		
 		return "redirect:selectAll.action";
 		
 	}
+	
 	@RequestMapping("/updateschedule")
-	public String updateschedule(Schedule schedule,Model model){
+	public String updateschedule(Schedule schedule, Model model){
 		scheduleService.updateschedule(schedule);
 		model.addAttribute("error", "更新成功");
 		model.addAttribute("schedule", schedule);
 		model.addAttribute("mainPage", "updateschedule.jsp");
+		
 		return "admin/main1";
 		
 	}
+	
 	@RequestMapping("/toinsert")
 	public String toinsert(Model model){
 		model.addAttribute("mainPage", "addschedule.jsp");
@@ -59,8 +69,9 @@ public class ScheduleController {
 		return "admin/main1";
 		
 	}
+	
 	@RequestMapping("/toupdate")
-	public String toupdate(Model model,Integer id){
+	public String toupdate(Model model, Integer id){
 		Schedule schedule=scheduleService.selectbyid(id);
 		model.addAttribute("schedule", schedule);
 		
