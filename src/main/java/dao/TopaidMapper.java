fb36452eac0eed114f6dca76259ce120c4ac278b
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:54:49 +0800

    Update TopaidMapper.java

--- src/main/java/dao/TopaidMapper.java
+++ src/main/java/dao/TopaidMapper.java
@@ -6,8 +6,13 @@ import pojo.QueryVo;
 import pojo.Topaid;
 
 public interface TopaidMapper {
+	
 	public void inserttopaid(Topaid topaid);
+	
 	public List<Topaid> findtopaid(QueryVo vo);
+	
 	public Topaid findbyid(Integer id);
+	
 	public void deletetopaid(Integer id);
+	
 }
