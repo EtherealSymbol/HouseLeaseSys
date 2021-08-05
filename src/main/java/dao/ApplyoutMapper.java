92a441ca5378c534104691447b89a931bb59e464
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:54:03 +0800

    Update ApplyoutMapper.java

--- src/main/java/dao/ApplyoutMapper.java
+++ src/main/java/dao/ApplyoutMapper.java
@@ -5,10 +5,17 @@ import java.util.List;
 import pojo.Applyout;
 
 public interface ApplyoutMapper {
+	
 	public void insertapplyout(Applyout applyout);
+	
 	List<Applyout> findallapplyout();
+	
 	public void updateapplyout(Applyout applyout);
+	
 	public void updateapplyoutbyhouse(Applyout applyout);
+	
 	public Applyout findbyid(Integer id);
+	
 	public void deleteapplyout(Integer id);
+	
 }
