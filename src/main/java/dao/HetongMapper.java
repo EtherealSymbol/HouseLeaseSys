ab5884e31f4f06d8f268cd973f57f22a2c01edf0
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:54:10 +0800

    Update HetongMapper.java

--- src/main/java/dao/HetongMapper.java
+++ src/main/java/dao/HetongMapper.java
@@ -5,7 +5,11 @@ import pojo.Hetong;
 public interface HetongMapper {
 	
 	public void inserthetong(Hetong hetong);
+	
 	public Hetong findhetong(String house_id);
+	
 	public void updatehetong(Hetong hetong);
+	
 	public void deletehetong(String house_id);
+	
 }
