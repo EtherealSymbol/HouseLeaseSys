bab468439b87ca6f7efa7246b01b8dd9190eedbe
EtherealSymbol <2391892926@qq.com>
2021-08-05 08:34:49 +0800

    Create SolveService.java

---
+++ src/main/java/service/SolveService.java
@@ -0,0 +1,16 @@
+package service;
+
+import java.util.List;
+
+import pojo.*;
+
+public interface SolveService {
+	public List<Solve> selectall(QueryVo vo);
+	public Integer selectcount(QueryVo vo);
+	public void deletesolve(Integer id);
+	
+	public List<Wrong> findwrong(QueryVo vo);
+	public Wrong findbyid(Integer id);
+	 public void insertwrong(Wrong wrong);
+	 public void gotosolve(Integer id, Solve solve);
+}
