6c9b21237ae8dfd13cf2b9eb25d0cf51e5b3200f
EtherealSymbol <2391892926@qq.com>
2021-08-05 08:34:57 +0800

    Create TopaidService.java

---
+++ src/main/java/service/TopaidService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+import pojo.*;
+
+public interface TopaidService {
+	public void inserttopaid(Topaid topaid);
+	public List<Topaid> findtopaid(QueryVo vo);
+	public Topaid findbyid(Integer id);
+	public void gotopay(Integer id,Paid paid);
+}
