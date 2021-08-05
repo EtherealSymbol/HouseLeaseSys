d1ae03ab1bace29fc0e402da680079e2eeeca211
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:53:24 +0800

    Update HetongController.java

--- src/main/java/controller/HetongController.java
+++ src/main/java/controller/HetongController.java
@@ -15,6 +15,7 @@ import service.ZulistService;
 @Controller
 @RequestMapping("/hetong")
 public class HetongController {
+	
 	@Autowired
 	private HetongService hetongService;
 	@Autowired
@@ -28,12 +29,13 @@ public class HetongController {
 	
 	//新增合同信息，修改房屋列表的状态，从申请列表中删除，增添到租赁列表当中
 	@RequestMapping("/inserthetong")
-	public String inserthetong(Model model,Hetong hetong){
+	public String inserthetong(Model model, Hetong hetong) {
+		
 		//新增合同信息
 		hetongService.inserthetong(hetong);
-		Hetong hetong1=hetongService.findhetong(hetong.getHouse_id());
+		Hetong hetong1 = hetongService.findhetong(hetong.getHouse_id());
 		//修改房屋列表状态
-		Houselist houselist=houselistService.findhouseid(hetong1.getHouse_id());
+		Houselist houselist = houselistService.findhouseid(hetong1.getHouse_id());
 		houselist.setStatus("已租赁");
 		houselistService.updatehousestatus(houselist);
 		//添加到租赁列表当中
@@ -48,33 +50,39 @@ public class HetongController {
 		//从申请列表中删除
 		applyService.deletebyhouse_id(hetong1.getHouse_id());
 		model.addAttribute("error", "zusuccess");
+		
 		return "redirect:/zulist/findzulist.action";
 		
 	}
 	@RequestMapping("/seehetong")
-	public String seehetong(String house_id,Model model){
+	public String seehetong(String house_id, Model model) {
 		Hetong hetong=hetongService.findhetong(house_id);
 		model.addAttribute("hetong", hetong);
 		model.addAttribute("mainPage", "hetong.jsp");
+		
 		return "admin/main1";
 	}
+	
 	@RequestMapping("/updatehetong")
-	public String updatehetong(String house_id,Model model){
+	public String updatehetong(String house_id, Model model) {
 		Hetong hetong=hetongService.findhetong(house_id);
 		model.addAttribute("hetong", hetong);
 		model.addAttribute("mainPage", "updatehetong.jsp");
+		
 		return "admin/main1";
 	}
+	
 	@RequestMapping("/changehetong")
-	public String changehetong(Hetong hetong){
+	public String changehetong(Hetong hetong) {
 		hetongService.updatehetong(hetong);
 		
 		return "redirect:/zulist/findzulist.action";
 	}
-	//终止合同操作：删除合同，插入已退租列表，删除在租列表，删除房屋列表
 	
+	//终止合同操作：删除合同，插入已退租列表，删除在租列表，删除房屋列表
 	@RequestMapping("/deletehetong")
-	public String deletehetong(String house_id,Model model){
+	public String deletehetong(String house_id, Model model) {
+		
 		hetongService.deletehetong(house_id);
 		Zulist zulist=zulistService.findzulist(house_id);
 		Checkout checkout=new Checkout();
@@ -87,14 +95,16 @@ public class HetongController {
 		zulistService.deletezulist(house_id);
 		
 		model.addAttribute("error", "checkoutsuccess");
+		
 		return "redirect:/zulist/findzulist.action";
 	}
 	
 	@RequestMapping("/zukeseehetong")
-	public String zukeseehetong(String house_id,Model model){
+	public String zukeseehetong(String house_id, Model model) {
 		Hetong hetong=hetongService.findhetong(house_id);
 		model.addAttribute("hetong", hetong);
 		model.addAttribute("mainPage", "showhetong.jsp");
+		
 		return "zuke/main";
 	}
 }
