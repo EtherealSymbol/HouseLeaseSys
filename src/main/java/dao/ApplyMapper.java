2bf2fb1d141359f661c45140d22d75ebc47f36ef
EtherealSymbol <2391892926@qq.com>
2021-08-05 15:53:56 +0800

    Update ApplyMapper.java

--- src/main/java/dao/ApplyMapper.java
+++ src/main/java/dao/ApplyMapper.java
@@ -6,9 +6,15 @@ import pojo.Apply;
 import pojo.Applyout;
 
 public interface ApplyMapper {
+	
 	void insertapply(Apply apply);
+	
 	public List<Apply> findapplylist() throws Exception;
+	
 	Apply findbyhouse_id(String house_id);
+	
 	public void deletebyhouse_id(String house_id);
+	
 	public void updateapplyout(Applyout applyout);
+	
 }
